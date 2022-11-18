4203a79cb19a9b831b9157278a4eee774038a60c
Romaric Zounlome <rzounlom@gmail.com>
2022-11-18 18:02:28 -0500

    add gradient layout to dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
+import GradientLayout from '../components/gradientLayout'
+
 const Home = () => {
-  return <div>home</div>
+  return (
+    <GradientLayout
+      color="blue"
+      subtitle="profile"
+      title="Romaric Zounlome"
+      description="15 public playlists"
+      roundImage
+    >
+      home
+    </GradientLayout>
+  )
 }
 
 export default Home
