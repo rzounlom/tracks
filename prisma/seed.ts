977e330c565eeb1a289ed28b9a23cd0cbc141886
Romaric Zounlome <rzounlom@gmail.com>
2022-11-18 07:09:08 -0500

    add signup api route

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,6 @@
-import { PrismaClient } from '@prisma/client'
 import { artistsData } from './songsData'
 import bcrypt from 'bcrypt'
-
-const prisma = new PrismaClient()
+import prisma from '../lib/prisma'
 
 const run = async () => {
   await Promise.all(
